3f15af50e3a2769506acd1ddf41ff361682ab7c2
Douglas Oliveira <oliveira.douglas@outlook.com>
2019-09-10 19:43:56 -0300

    utilizando Root import

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
-
-import Dashboard from '../pages/Dashboard';
-import Profile from '../pages/Profile';
-import SignIn from '../pages/SignIn';
-import SignUp from '../pages/SignUp';
+import Dashboard from '~/pages/Dashboard';
+import Profile from '~/pages/Profile';
+import SignIn from '~/pages/SignIn';
+import SignUp from '~/pages/SignUp';
 
 import Route from './Route';
 
