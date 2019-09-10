36ba51d8a331803a17853311016cd53f6daee9cb
Douglas Oliveira <oliveira.douglas@outlook.com>
2019-09-09 23:30:25 -0300

    rotas privadas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
+import Route from './Route';
+
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/profile" component={Profile} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
+      <Route path="/" component={() => <h1>404</h1>} />
     </Switch>
   );
 }
