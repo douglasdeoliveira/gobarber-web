d439063a4c99fb64cd793fc9ce5e35cb7103aa8d
Douglas Oliveira <oliveira.douglas@outlook.com>
2019-09-10 21:28:32 -0300

    estilizaçao da autenticaçao

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,8 +2,8 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 
-import AuthLayout from '../pages/_layouts/auth';
-import DefaultLayout from '../pages/_layouts/default';
+import AuthLayout from '~/pages/_layouts/auth';
+import DefaultLayout from '~/pages/_layouts/default';
 
 export default function RouteWrapper({
   component: Component,
