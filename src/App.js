cafbdf6a77aea878f59dd500301c510a678c9141
Douglas Oliveira <oliveira.douglas@outlook.com>
2019-09-10 23:58:25 -0300

    persistindo autenticaçao

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './config/ReactotronConfig';
 import Routes from './routes';
 import history from './services/history';
-import store from './store';
+import { persistor, store } from './store';
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <GlobalStyle />
-        <Routes />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <GlobalStyle />
+          <Routes />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
